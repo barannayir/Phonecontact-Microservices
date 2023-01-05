507fdb1c6bd9b3f1b349dbc1987227d2b4c602c0
barannayir <79968430+barannayir@users.noreply.github.com>
2023-01-05 22:12:09 +0300

    Add project files.

---
+++ ContactMicroService/Entities/Contact.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace ContactMicroService.Entities
+{
+    public class Contact
+    {
+        [BsonId]
+        public string uuid { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Firma { get; set; }
+        public List<ContactInformation> ContactInformation { get; set; }
+
+    }
+
+    public class ContactInformation
+    {
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Location { get; set; }
+    }
+}
