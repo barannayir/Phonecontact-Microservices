d2253536f2c7315c4bd7352651f76413c6119a43
barannayir <79968430+barannayir@users.noreply.github.com>
2023-01-18 23:43:33 +0300

    Report Test Added

---
+++ ReportServiceTests/Controllers/ReportControllerTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReportService.Controllers;
+using ReportService.Entities.Dtos;
+using ReportService.Repositories.Interfaces;
+using ReportServiceTests.Services;
+using Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportService.Controllers.Tests
+{
+    [TestClass()]
+    public class ReportControllerTests
+    {
+        private readonly IReportRepository _reportService;
+        private readonly IRabbitMQPublisherService _rabbitMQPublisherService;
+        private readonly ReportController _controller;
+
+        public ReportControllerTests()
+        {
+            _reportService = new ReportTestService();
+            _rabbitMQPublisherService = new RabitMQTestService();
+            _controller = new ReportController(_reportService, _rabbitMQPublisherService);
+        }
+
+        [TestMethod()]
+        public async Task GetAllTest()
+        {
+            var actionResult = await _controller.GetAll();
+            var okResult = actionResult as ObjectResult;
+            var actualConfiguration = okResult.Value as Response<List<ReportDto>>;
+            Assert.IsNotNull(actualConfiguration);
+            Assert.IsTrue(actualConfiguration.IsSuccessful);
+            Assert.IsNotNull(actualConfiguration.Data);
+        }
+
+        [TestMethod()]
+        public async Task GetByIdTest()
+        {
+            var actionResult = await _controller.GetById("63c849534431c7d93359246f");
+            var okResult = actionResult as ObjectResult;
+            var actualConfiguration = okResult.Value as Response<ReportDto>;
+            Assert.IsNotNull(actualConfiguration);
+            Assert.IsTrue(actualConfiguration.IsSuccessful);
+            Assert.IsNotNull(actualConfiguration.Data);
+        }
+
+        [TestMethod()]
+        public async Task UpdateTest()
+        {
+            ReportDto report = new ReportDto { Id = "63c849534431c7d93359246f", CreatedDate = DateTime.Now.AddHours(-1), FilePath = "TestPath1", Status = ReportService.Entities.ReportStatusType.COMPLETED };
+            var actionResult = await _controller.Update(report);
+            var okResult = actionResult as ObjectResult;
+            var actualConfiguration = okResult.Value as Response<NoContent>;
+            Assert.IsNotNull(actualConfiguration);
+            Assert.IsTrue(actualConfiguration.IsSuccessful);
+        }
+    }
+}
---
+++ ReportServiceTests/Services/RabitMQTestService.cs
@@ -0,0 +1,18 @@
+﻿using ReportService.Entities;
+using ReportService.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportServiceTests.Services
+{
+    internal class RabitMQTestService : IRabbitMQPublisherService
+    {
+        public void Publish(CreateReportEvent createReportEvent, string queue, string routing, string exchange)
+        {
+
+        }
+    }
+}
